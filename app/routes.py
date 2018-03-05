b4b42a0a56872b2751ceb0f10702621ce69d946f
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-03-05 14:06:29 -0700

    python simple server running

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from app import app
+
+@app.route("/")
+def index():
+    return "Index!"
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+@app.route("/members")
+def members():
+    return "Members"
+
+@app.route("/members/<string:name>/")
+def getMember(name):
+    return name

abe7133f7163eb2c02e023fabf446388024f9132
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-03-08 20:26:42 -0700

    judah

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_migrate import Migrate
+from webargs import fields
 from flask_apispec import use_kwargs, marshal_with
 from flask_marshmallow import Marshmallow
-from webargs import fields
 
 
 app = Flask(__name__)

9bd4cd0f408a047b4fa0a403ebe68d1e9e863ad8
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-03-08 22:20:36 -0700

    judah

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
-import psycopg2
-DATABASE_URL = os.environ['postgres://localhost/capstone']
-conn = psycopg2.connect(DATABASE_URL, sslmode='postgres://localhost/capstone')
+# import psycopg2
+# DATABASE_URL = os.environ['postgres://localhost/capstone']
+# conn = psycopg2.connect(DATABASE_URL, sslmode='postgres://localhost/capstone')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgres://localhost/capstone'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.envron['postgres://localhost/capstone']
     SQLALCHEMY_TRACK_MODIFICATIONS = False

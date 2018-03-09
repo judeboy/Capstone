4da99f53b0cf84b017a4b9d1aa6c77c19a9fc1ef
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-03-08 20:39:27 -0700

    judah

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_migrate import Migrate
-from webargs import fields
-from flask_apispec import use_kwargs, marshal_with
+# from flask_apispec import use_kwargs, marshal_with
+# from webargs import fields
 from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
 
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgres://localhost/capstone'
+    SQLALCHEMY_DATABASE_URI = 'postgres://localhost/capstone'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

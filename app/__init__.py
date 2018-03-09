a35b1a1ff80cc0b56542e758a3852c9a318498ed
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-03-08 20:41:51 -0700

    judah

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from config import Config
 from flask_migrate import Migrate
 # from flask_apispec import use_kwargs, marshal_with
 # from webargs import fields
-from flask_marshmallow import Marshmallow
+# from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)

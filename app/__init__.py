1655382d6f74737cf124c709b8b000b609f920ea
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-03-08 20:21:49 -0700

    judah

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,19 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 from config import Config
 from flask_migrate import Migrate
 from webargs import fields
 from flask_apispec import use_kwargs, marshal_with
-
-
-
+from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
 db = SQLAlchemy(app)
+app.config.from_object(Config)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
+
+
+
 
 from app import routes, models

44bd5f2ab64846a37cf0da7d71582ace3cb2e661
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-03-09 12:42:56 -0700

    GET all working on HEROKU!!

--- app/routes.py
+++ app/routes.py
@@ -9,21 +9,20 @@ from flask import jsonify
 
 @app.route("/")
 def get_progs():
-    # return "hello world"
     all_programs = Programs.query.all()
     result = programs_schema.dump(all_programs)
     return jsonify(result.data)
 
-# @app.route("/api/sean")
-# def get_sean():
-#     all_programs = Programs.query.all()
-#     result = programs_schema.dump(all_programs)
-#     arr = []
-#     length = len(result[0])
-#     for i in result[0]:
-#         arr.append(i['GovAgency'])
-#     return jsonify(arr)
-#
+@app.route("/api/sean")
+def get_sean():
+    all_programs = Programs.query.all()
+    result = programs_schema.dump(all_programs)
+    arr = []
+    length = len(result[0])
+    for i in result[0]:
+        arr.append(i['GovAgency'])
+    return jsonify(arr)
+
 # @app.route("/api/gov_agencies")
 # @use_kwargs({'GovAgency': fields.Str()})
 # @marshal_with(programs_schema)

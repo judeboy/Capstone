7ef18c3d784bd918e43478b3f6c1f6ee0a73979b
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-03-09 12:05:38 -0700

    database url set?

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgres://desyrgytwnqqsz:b0fbf3976ab15ee0e05844a889459d0e4fefe2be0cf1ee1e8a2ed12f02d992b9@ec2-54-235-66-24.compute-1.amazonaws.com:5432/d5bmadat7p5o27') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'postgres://localhost/capstone'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

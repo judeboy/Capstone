9486354de03770a872659f40cd12e1fffec32616
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-03-09 11:20:59 -0700

    moving imports around

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify
 from config import Config
+from flask_apispec import use_kwargs, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+
 # from webargs import fields
-# from flask_apispec import use_kwargs, marshal_with
 
 
 app = Flask(__name__)
